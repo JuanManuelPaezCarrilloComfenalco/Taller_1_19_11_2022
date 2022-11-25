92d8b2504c0202df83add23c88183e4c6cffec2b
JuanManuelPaezCarrilloComfenalco <paez3939@gmail.com>
2022-11-24 21:43:53 -0500

    solucion 5

---
+++ pedir_datos_por_promt_19_11_2022/main.js
@@ -0,0 +1,10 @@
+addEventListener('DOMContentLoaded', (e)=>{
+
+    let numero= prompt('ingresar numero');
+    if (numero !=null){
+     alert('el numero que ingreso es_' + numero);
+    }
+    else{
+     alert('introduce un numero para continuar');
+    }
+ })
